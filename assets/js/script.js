c7a1e5089466a30e1ec496addaece619fa6aa230
MarkwSch <131507173+MarkwSch@users.noreply.github.com>
2023-11-18 18:53:29 +0800

    feat: create event listener and api call for new form

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+document.getElementById('submitButton').addEventListener('click', function(){
+
+    // Retrieve the inputted word
+    var word = document.getElementById('wordInput').value;
+    // Get the tag where the results will be outputted
+    var resultP = document.getElementById('result');
+    var url = 'https://api.api-ninjas.com/v1/dictionary?word=' + word;
+    console.log(word)
+     // Make the API call
+     const result = fetch(url, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': 'rFXNl/iLr9XfX+7F1f51ag==cgw1tl3gsL0kszcn',
+          'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Write to console
+        console.log(data);
+        // Set def as the definition
+        var def = data.definition
+        console.log(def)
+        // If the definition is null or empty, print "No definition found."
+        if(def == "") {
+            var formattedDef = "No definition found."
+        }
+        // Else break up the definition at "1." so every new meaning is on a separate line.
+        else {
+            var formattedDef = def.split('\n1.').join('<br><br>1.');
+        }
+        console.log(def)
+        // Output in defP
+        resultP.innerHTML = formattedDef
+    })
+}); 
